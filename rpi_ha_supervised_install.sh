265e54bf6ebda8beef2a623f2040d3044e3d430a
allanbell3d <36531528+allanbell3d@users.noreply.github.com>
2020-11-16 23:47:05 +0400

    Update rpi_ha_supervised_install.sh

--- rpi_ha_supervised_install.sh
+++ rpi_ha_supervised_install.sh
@@ -1,11 +1,8 @@
 # Script to install Home Assistant supervised in raspberry
-version=$1
-
-
 sudo -i
 apt-get install -y software-properties-common apparmor-utils apt-transport-https avahi-daemon ca-certificates curl dbus jq network-manager socat
 systemctl disable ModemManager
 systemctl stop ModemManager
 curl -fsSL get.docker.com | sh
-curl -sL "https://raw.githubusercontent.com/Kanga-Who/home-assistant/master/supervised-installer.sh" | bash -s -- -m raspberrypi"$version"
+curl -sL "https://raw.githubusercontent.com/Kanga-Who/home-assistant/master/supervised-installer.sh" | bash -s -- -m raspberrypi4
 exit
