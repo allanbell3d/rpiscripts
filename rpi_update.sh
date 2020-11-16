a2eb75d6b3653870a00c7f9e360281957229b464
allanbell3d <36531528+allanbell3d@users.noreply.github.com>
2020-11-16 20:19:16 +0400

    Update rpi_update.sh

--- rpi_update.sh
+++ rpi_update.sh
@@ -2,6 +2,7 @@ sudo timedatectl set-timezone Asia/Dubai
 # sudo raspi-config nonint do_hostname blenode
 sudo apt update -y
 sudo apt full-upgrade -y
+sudo apt autoremove -y
 sudo apt clean -y
 sudo rpi-eeprom-update -a
 sudo reboot
