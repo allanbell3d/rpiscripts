8bc234f697cc3672e1b9992e2eb66d0d6887894e
allanbell3d <36531528+allanbell3d@users.noreply.github.com>
2020-11-19 02:44:43 +0400

    Update rpi_update.sh

--- rpi_update.sh
+++ rpi_update.sh
@@ -6,6 +6,7 @@ sudo apt update -y
 sudo apt full-upgrade -y
 sudo apt autoremove -y
 sudo apt clean -y
+sudo apt-get -y install git
 sudo rpi-eeprom-update -a
 echo Update finished, rebooting
 sudo reboot
