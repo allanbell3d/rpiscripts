7f92e7bd1f7971bcb44245da46371e6c359967cb
allanbell3d <36531528+allanbell3d@users.noreply.github.com>
2020-11-21 14:04:33 +0400

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 sudo docker volume create grafana_data
-sudo docker run -d -p 3000:3000 --name=grafana --restart=always -v grafana_data:/var/lib/grafana -e "GF_INSTALL_PLUGINS=natel-discrete-panel,blackmirror1-singlestat-math-panel" grafana/grafana 
+sudo docker run -d -p 3000:3000 --name=grafana --restart=unless-stopped -v grafana_data:/var/lib/grafana -e "GF_INSTALL_PLUGINS=natel-discrete-panel,blackmirror1-singlestat-math-panel" grafana/grafana 
